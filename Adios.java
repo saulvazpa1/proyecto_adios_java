11919badcb171e29eb04a9adb095cab6706265fd
saulvazpa1 <saulvaz.pa@gmail.com>
2026-05-18 09:47:50 -0600

    Metodo multiplicar

--- Adios.java
+++ Adios.java
@@ -5,6 +5,9 @@ public class Adios
     public static int restar(int numero, int numero2) {
     return numero- numero2;
 }
+public static int multiplicar(int n1,int n2){
+    return  n1*n2;
+}
     public static void main(String[] args ){
        int numero1,numero2;
         Scanner scanner = new Scanner(System.in);
